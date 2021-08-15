61e42a5a3bc2b18b7f037d923bdb2f97671dde26
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-08-15 23:26:57 +0600

    function update

--- transactions.js
+++ transactions.js
@@ -5,26 +5,40 @@ loginButton.addEventListener('click',function(event){
     loginSection.style.display='none';
     moneySection.style.display='block';
 })
+
 const depositButton = document.getElementById('deposit-btn');
 const withdrawButton = document.getElementById('withdraw-btn');
-const depositInput = document.getElementById('deposit-input');
-const withdrawInput = document.getElementById('withdraw-input');
-const depositValue = document.getElementById('deposit-value');
-const withdrawValue = document.getElementById('withdraw-value');
-const banalceValue = document.getElementById('balance-value')
-depositButton.addEventListener('click',function(event){
-    var depositAmount = parseFloat(document.getElementById('deposit-input').value);
-    var newDeposit = parseFloat(depositValue.innerText) + depositAmount;
-    depositValue.innerText = newDeposit.toFixed(2);
-    var newBalance = parseFloat(banalceValue.innerText) + depositAmount;
-    banalceValue.innerText = newBalance.toFixed(2);
-    depositInput.value = "";
+function getInputValue(id)
+{
+    return parseFloat(document.getElementById(id).value);
+}
+function getValue(targetId)
+{
+    return parseFloat(document.getElementById(targetId).innerText);
+}
+function setValue(val,targetId)
+{
+    document.getElementById(targetId).innerText = val.toFixed(2);
+}
+function clearInput(id)
+{
+    document.getElementById(id).value = '';
+}
+depositButton.addEventListener('click',function(event)
+{
+    let deposit = getInputValue('deposit-input');
+    let newDeposit = getValue('deposit-value') + deposit;
+    setValue(newDeposit,'deposit-value');
+    let newBalance = getValue('balance-value') + deposit;
+    setValue(newBalance,'balance-value');
+    clearInput('deposit-input');
 })
-withdrawButton.addEventListener('click',function(event){
-    var withdrawAmount = parseFloat(document.getElementById('withdraw-input').value);
-    var newWithdraw = parseFloat(withdrawValue.innerText) + withdrawAmount;
-    withdrawValue.innerText = newWithdraw.toFixed(2);
-    var newBalance = parseFloat(banalceValue.innerText) - withdrawAmount;
-    banalceValue.innerText = newBalance.toFixed(2);
-    withdrawInput.value = "";   
+withdrawButton.addEventListener('click',function(event)
+{
+    let withdraw = getInputValue('withdraw-input');
+    let newWithdraw = getValue('withdraw-value') + withdraw;
+    setValue(newWithdraw,'withdraw-value');
+    let newBalance = getValue('balance-value') - withdraw;
+    setValue(newBalance,'balance-value');
+    clearInput('withdraw-input');
 })
